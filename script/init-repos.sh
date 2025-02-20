e647cf40b5580d7152339c18592d485905433ab0
edoardo.caon <edoardo.caon@orbyta.it>
2025-02-20 11:47:38 +0100

    infra: add script to clone and manage repositories

---
+++ script/init-repos.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+PRINCIPAL_DIR="influenza-workspace"
+
+
+REPOS=(
+    "https://orbyta-isi-foundation@dev.azure.com/orbyta-isi-foundation/influenzanet/_git/user-management-service"
+    "https://dev.azure.com/orbyta-isi-foundation/influenzanet/_git/study-service"
+    "https://dev.azure.com/orbyta-isi-foundation/influenzanet/_git/messaging-service"
+    "https://dev.azure.com/orbyta-isi-foundation/influenzanet/_git/logging-service"
+    "https://dev.azure.com/orbyta-isi-foundation/influenzanet/_git/infra"
+    "https://dev.azure.com/orbyta-isi-foundation/influenzanet/_git/api-gateway"
+    "https://dev.azure.com/orbyta-isi-foundation/influenzanet/_git/participant-webapp"
+)
+
+
+mkdir -p "$PRINCIPAL_DIR"
+cd "$PRINCIPAL_DIR" || exit 1
+
+for REPO in "${REPOS[@]}"; do
+    REPO_NAME=$(basename -s .git "$REPO")
+    if [ -d "$REPO_NAME" ]; then
+        echo "Repository $REPO_NAME already exists, skipping..."
+    else
+        git clone "$REPO" "$REPO_NAME" && cd "$REPO_NAME"
+        if [[ "$REPO_NAME" == *infra* ]]; then
+            git checkout main
+        else
+            git checkout master
+        fi
+        cd ..
+    fi
+done
+
+echo "Script executed successfully! All repositories have been processed."
